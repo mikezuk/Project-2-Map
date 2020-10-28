db7f8f748031388bbeac79a8293b39b3e88fbc5d
mikezuk <72841801+mikezuk@users.noreply.github.com>
2020-10-28 00:16:07 -0300

    2nd c

--- sketch.js
+++ sketch.js
@@ -2,13 +2,16 @@ let img;
 function preload() {
   img = loadImage('map.png');
   img3 = loadImage ('pin2.png');
- // img2 = loadImage ('水')
+  myFont = loadFont('selima_.otf');
+ img2 = loadImage ('water.gif')
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
   //image(img2, 0, 0);
+
   image (img, 0, 0 );
   imageMode (CENTER)
+  image (img2,width/2,height - 90 ,500, 200)
   tint (0,190)
   image (img3, 1303, 237, width/21,height/12,)
   image (img3, 387, 216, width/21,height/12,)
@@ -16,7 +19,12 @@ function setup() {
   
 
 }
-
+function draw () {
+  textSize(80);
+  textFont (myFont)
+  textAlign (CENTER)
+text('Point & Go!', width/2, height /2);
+}
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
