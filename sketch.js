4f002b8c37d275e99df134660cb22a26f621fb6b
mikezuk <72841801+mikezuk@users.noreply.github.com>
2020-10-29 14:21:35 -0300

    1

--- sketch.js
+++ sketch.js
@@ -1,29 +1,39 @@
 let img;
+let canvas2;
+
 function preload() {
   img = loadImage('map.png');
   img3 = loadImage ('pin2.png');
   myFont = loadFont('selima_.otf');
- img2 = loadImage ('water.gif')
+  img4 = loadImage ('plane.png')
+ img2 = createImg('water.gif');
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  //image(img2, 0, 0);
+  background (255)
+//   canvas2 = createGraphics (windowWidth, windowHeight)
+// canvas2.img4 (0)
 
+img2.position (300,400,50,20);
   image (img, 0, 0 );
   imageMode (CENTER)
-  image (img2,width/2,height - 90 ,500, 200)
+ 
   tint (0,190)
   image (img3, 1303, 237, width/21,height/12,)
   image (img3, 387, 216, width/21,height/12,)
   image (img3, 726, 190, width/21,height/12,)
   
-
+  
+  
+  
 }
 function draw () {
   textSize(80);
   textFont (myFont)
   textAlign (CENTER)
 text('Point & Go!', width/2, height /2);
+//image (img4, mouseX, mouseY)
+
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
